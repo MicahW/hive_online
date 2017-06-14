428b9dcc5607403dad86cb821521ef1b74cfbde3
MicahW <micah.wallberg@gmail.com>
2017-06-14 13:13:12 -0400

    add link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

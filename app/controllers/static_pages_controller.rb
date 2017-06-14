da1a9430641bf97a8cb7a99fb49091805ff788e3
MicahW <micah.wallberg@gmail.com>
2017-06-14 13:48:25 -0400

    active bar now dynamic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
+  
+  
+  
   def home
+    @hash = Hash.new("")
+    @hash[:home] = "active"
   end
 
   def contact
+    @hash = Hash.new("")
+    @hash[:contact] = "active"
   end
 end

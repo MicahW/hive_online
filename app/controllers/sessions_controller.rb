8bd059e130c251f3854588354012bfa962698278
MicahW <micah.wallberg@gmail.com>
2017-06-20 09:21:18 -0400

    added sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    @flash = Hash.new("")
+    @flash[:danger] = false
+    @hash = Hash.new("")
+    @hash[:login] = "active"
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      @hash = Hash.new("")
+      @flash = Hash.new("")
+      @flash[:danger] = true
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   get 'user/show'
-
   get 'user/new'
 
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'user#new'
   post '/signup', to: 'user#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   resources :user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

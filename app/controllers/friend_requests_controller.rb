c5a774a998ee5e1aee004bb7d1f4503bfaf6b8f4
MicahW <micah.wallberg@gmail.com>
2017-08-09 13:48:39 -0400

    now can have dual requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,7 +3,8 @@ class FriendRequestsController < ApplicationController
   def create
     @sender = current_user
     @user = User.find(params[:user_id])
-    if !@user.friend_request.exists?(from_id: @sender.id)
+    if !@user.friend_request.exists?(from_id: @sender.id) and
+       !@sender.friend_request.exists?(from_id: @user.id)
       @user.friend_request.create(from_id: @sender.id)
     end
     redirect_to index_path
--- test/integration/sending_friend_requests_test.rb
+++ test/integration/sending_friend_requests_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class SendingFriendRequestsTest < ActionDispatch::IntegrationTest
   def setup
     @bob = users(:bob)
-    @user = users(:michael)
+    @michael = users(:michael)
     @admin = users(:admin)
     @cracker = users(:cracker)
   end
   
  test "send friend request adds to db, but second dose not" do
-   post login_path, params: { session: { email:    @user.email,
+   post login_path, params: { session: { email:    @michael.email,
                                           password: 'password' } }
    get index_path
    assert_difference '@bob.friend_request.count', 1 do
@@ -23,5 +23,15 @@ class SendingFriendRequestsTest < ActionDispatch::IntegrationTest
     post user_friend_requests_path(@bob)
     assert_redirected_to index_path
    end
+   delete logout_path
+   
+   #now bob should not be able to send friend request to michael
+   post login_path, params: { session: { email:    @bob.email,
+                                          password: 'password' } }
+   
+   #second request should not add another one
+   assert_no_difference '@michael.friend_request.count' do
+    post user_friend_requests_path(@michael)
+   end  
  end
 end

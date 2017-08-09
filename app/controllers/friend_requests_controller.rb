8d7fac5de74c4e794501aaccbcece177f140ee7a
MicahW <micah.wallberg@gmail.com>
2017-08-09 13:21:43 -0400

    tested for friend requests, now add requests to user page

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,8 +3,9 @@ class FriendRequestsController < ApplicationController
   def create
     @sender = current_user
     @user = User.find(params[:user_id])
-    puts @sender.id.class
-    @user.friend_request.create(from_id: @sender.id)
+    if !@user.friend_request.exists?(from_id: @sender.id)
+      @user.friend_request.create(from_id: @sender.id)
+    end
     redirect_to index_path
   end
   
--- test/integration/sending_friend_requests_test.rb
+++ test/integration/sending_friend_requests_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SendingFriendRequestsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bob = users(:bob)
+    @user = users(:michael)
+    @admin = users(:admin)
+    @cracker = users(:cracker)
+  end
+  
+ test "send friend request adds to db, but second dose not" do
+   post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+   get index_path
+   assert_difference '@bob.friend_request.count', 1 do
+    post user_friend_requests_path(@bob)
+    assert_redirected_to index_path
+    follow_redirect!
+   end
+   
+   #second request should not add another one
+   assert_no_difference '@bob.friend_request.count' do
+    post user_friend_requests_path(@bob)
+    assert_redirected_to index_path
+   end
+ end
 end
